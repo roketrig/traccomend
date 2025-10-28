cadb6e46cd4919931a69aa896b92ccb6828c16df
Bugra kasarci <b1m.ksr@yahoo.com>
2025-10-28 14:15:53 +0300

    file update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { appConfig } from './app/app.config'; // ← appConfig import ediliyor mu?
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, appConfig) // ← appConfig kullanılıyor mu?
   .catch((err) => console.error(err));
