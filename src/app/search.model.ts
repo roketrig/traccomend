d881c206f63abb549e29983bfaaaa669e61f92a2
Sudexq <sudenur.tilla2020@gmail.com>
2025-11-13 10:22:12 +0300

    added search model

---
+++ src/app/search.model.ts
@@ -0,0 +1,10 @@
+export interface search {
+    target_city:string,
+    target_city_iata_code:string,
+    origin_country:string,
+    departure_city:string,
+    departure_city_iata_code:string,
+    adult:number,
+    departure_date:string,
+    return_date:string,
+}
